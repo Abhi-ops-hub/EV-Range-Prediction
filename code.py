d6937cef4cdd97fa60d978a30272c6a61c578dcf
Abhishek goswami <abhi.pvt2523@gmail.com>
2026-02-11 01:46:19 +0530

    Top 20 Power consumption at what speed

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ df=pd.read_csv(r"EV_Range_Prediction_Dataset_2000.csv")
 
 # data cleaning..deleting the missing data
 
-df=df.dropna(subset=['battery_soc','remaining_range'])
+df=df.dropna(subset=['battery_soc','power_consumption','speed','remaining_range'])
 
 # plotting battery SoC and Remaining range
 
@@ -15,5 +15,19 @@ plt.scatter(df['battery_soc'],df['remaining_range'])
 plt.title('EV-remaining range(KM) VS battery SoC')
 plt.xlabel('battery_soc')
 plt.ylabel('remainig_range')
-plt.legend()
+plt.savefig("soc vs remaining range.png",dpi=300)
+plt.show()
+
+# now for power consumption
+
+plt.figure(figsize=(8,6))
+top_power=df.sort_values('power_consumption',ascending=False).head(20)
+plt.scatter(top_power['speed'],top_power['power_consumption'],color="k",marker="o")
+plt.xlabel("speed")
+plt.ylabel("Power consumption")
+plt.title("Top 20 Power Consumption at speed")
+
+plt.xticks()
+
+plt.grid()
 plt.show()
